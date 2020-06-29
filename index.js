b4016244769d8571f688bf5a78e50cffbc258332
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-06-29 16:45:47 +0900

    feat: accept arguments using yargs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+require('yargs')
+  .command('get-record', 'get 1 record', (yargs) => {
+    yargs
+      .positional('app', {
+        describe: 'The ID of the app',
+      })
+      .positional('record', {
+        describe: 'The ID of the record',
+      })
+    }, argv => {
+      console.log('argv', argv);
+    })
+    .argv
